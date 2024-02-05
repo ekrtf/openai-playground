ef9c951228cffd55fc999e144e2b1f24c0882b8b
Emile Kratiroff <emile@kratiroff.com>
2024-02-05 14:45:51 +0100

    Fetch list of engines

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -16,5 +19,22 @@ func main() {
 
 	apiKey := os.Getenv("OPENAI_API_KEY")
 
-	fmt.Println(apiKey)
+	req, err := http.NewRequest(http.MethodGet, "https://api.openai.com/v1/engines", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	resp, err := client.Do(req)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(body))
 }
